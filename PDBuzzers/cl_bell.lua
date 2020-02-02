0cb13632f56fd43fe93ac73cbca5f8b48364d655
Rman723 <rhett.thacker@gmail.com>
2020-02-02 11:41:43 -0700

    Update cl_bell.lua

--- PDBuzzers/cl_bell.lua
+++ PDBuzzers/cl_bell.lua
@@ -1,7 +1,17 @@
 local player = GetPlayerPed(-1)
+
 local dings = 0
+
 local times = 5000 --This changes the wait time between buzzes
+
 local radius = 0 --Radius the buzz can be heard from
+
+-- Dont Recommend changing anything below this line --
+
+local audioName = "DOOR_BUZZ"
+
+local audioRef = "MP_PLAYER_APARTMENT"
+
 -- Mission Row PD
 Citizen.CreateThread(function()
     while true do 
@@ -9,15 +19,17 @@ Citizen.CreateThread(function()
             if GetDistanceBetweenCoords(439.07333374023, -981.12860107422, 30.689603805542, GetEntityCoords(player)) < 0.5 then
                 drawTxt('Press ~g~L~s~ to buzz', 2, 1, 0.5, 0.8, 0.6, 255, 255, 255, 255)
             if IsControlJustReleased(1, 182) then
-                PlaySoundFromCoord(-1, "DOOR_BUZZ", 439.07333374023, -981.12860107422, 30.689603805542, "MP_PLAYER_APARTMENT", 0, radius, 0)
-                dings = dings +1 
-                Citizen.Wait(times)
-                dings = dings -1
-
+                if GetDistanceBetweenCoords(439.07333374023, -981.12860107422, 30.689603805542, GetEntityCoords(player)) < 35 then
+                    PlaySoundFrontend(-1, audioName, audioRef, 0, 0, 1)
+                    dings = dings +1 
+                    Citizen.Wait(times)
+                    dings = dings -1
+                end
             end
         end
     end
 end)
+
 -- Sandy Shores SO
 Citizen.CreateThread(function()
     while true do 
@@ -25,15 +37,17 @@ Citizen.CreateThread(function()
             if GetDistanceBetweenCoords(1852.9208984375, 3687.6489257813, 34.276844024658, GetEntityCoords(player)) < 0.5 then
                 drawTxt('Press ~g~L~s~ to buzz', 2, 1, 0.5, 0.8, 0.6, 255, 255, 255, 255)
             if IsControlJustReleased(1, 182) then
-                PlaySoundFromCoord(-1, "DOOR_BUZZ", 1852.9208984375, 3687.6489257813, 34.276844024658, "MP_PLAYER_APARTMENT", 0, radius, 0)
-                dings = dings +1 
-                Citizen.Wait(times)
-                dings = dings -1
-
+                if GetDistanceBetweenCoords(1852.9208984375, 3687.6489257813, 34.276844024658, GetEntityCoords(player)) < 35 then
+                    PlaySoundFrontend(-1, audioName, audioRef, 0, 0, 1)
+                    dings = dings +1 
+                    Citizen.Wait(times)
+                    dings = dings -1
+                end
             end
         end
     end
 end)
+
 -- Paleto Bay SO
 Citizen.CreateThread(function()
     while true do 
@@ -41,11 +55,12 @@ Citizen.CreateThread(function()
             if GetDistanceBetweenCoords(-447.07751464844, 6013.0073242188, 31.716390609741, GetEntityCoords(player)) < 0.5 then
                 drawTxt('Press ~g~L~s~ to buzz', 2, 1, 0.5, 0.8, 0.6, 255, 255, 255, 255)
             if IsControlJustReleased(1, 182) then
-                PlaySoundFromCoord(-1, "DOOR_BUZZ", -447.07751464844, 6013.0073242188, 31.716390609741, "MP_PLAYER_APARTMENT", 0, radius, 0)
-                dings = dings +1 
-                Citizen.Wait(times)
-                dings = dings -1
-
+                if GetDistanceBetweenCoords(-447.07751464844, 6013.0073242188, 31.716390609741, GetEntityCoords(player)) < 35 then
+                    PlaySoundFrontend(-1, audioName, audioRef, 0, 0, 1)
+                    dings = dings +1 
+                    Citizen.Wait(times)
+                    dings = dings -1
+                end
             end
         end
     end
